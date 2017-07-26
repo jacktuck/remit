3a9c3b672259b5d51e86da52866bb46582fa1240
Jack Tuck <jack.tuck@crugo.com>
2017-07-26 18:33:47 +0100

    You can not bind a function using a getter

--- test/lib/QueueInfo.test.js
+++ test/lib/QueueInfo.test.js
@@ -1,30 +1,78 @@
 /* global describe, it, expect, sinon, remit */
-describe('queueInfo', function () {
-  it('should return null if not found', function (done) {
-    remit.request('foo')
-      .queueInfo()
-      .then((result) => {
-        expect(result).to.equal(null)
-        done()
-      })
-      .catch(err => {
-        done(err)
-      })
-  })
+// describe('queueInfo', function () {
+//   it('should return null if not found', function (done) {
+//     remit.req('foo')
+//     .options({
+//       .queueInfo()
+//       .then((result) => {
+//         expect(result).to.equal(null)
+//         done()
+//       })
+//       .catch(err => {
+//         done(err)
+//       })
+//   })
+//
+//   it('should return queue info if found', function (done) {
+//       remit.res('baz').ready()
+//       .then(() => {
+//         remit.req('baz')
+//         .options({
+//         .queueInfo()
+//         .then((result) => {
+//           const expected = { queue: 'baz', messageCount: 0, consumerCount: 1 }
+//           expect(result).to.deep.equal(expected)
+//           done()
+//         })
+//         .catch(err => {
+//           done(err)
+//         })
+//     })
+//   })
+// })
 
+describe('queueInfo', function () {
+  // it('should return null if not found', function (done) {
+  //   remit.emit('foobar')
+  //   .options({ delay: 6e5 })
+  //     .queueInfo()
+  //     .then((result) => {
+  //       expect(result).to.equal(null)
+  //       done()
+  //     })
+  //     .catch(err => {
+  //       done(err)
+  //     })
+  // })
+  //
   it('should return queue info if found', function (done) {
-      remit.res('baz').ready()
-      .then(() => {
-        remit.req('baz')
-        .queueInfo()
-        .then((result) => {
-          const expected = { queue: 'baz', messageCount: 0, consumerCount: 1 }
-          expect(result).to.deep.equal(expected)
-          done()
-        })
-        .catch(err => {
-          done(err)
-        })
+    let requestToMake = remit
+      .emit('bazfoo')
+      .options({ delay: 6e5 })
+
+    remit.listen('bazfoo').ready().then(() => {
+      console.log('SENDING REQUEST')
+      return requestToMake.send()
+    })
+    .then(() => {
+      console.log('SENDING Q INFO')
+      return requestToMake.queueInfo()
+    })
+    .then((result) => {
+      console.log('GOT QUEUE INFO', result)
+
+      const expected = {
+        queue: 'd:remit:bazfoo:6000',
+        messageCount: 0,
+        consumerCount: 1
+      }
+
+      expect(result).to.not.equal(null)
+      expect(result).to.deep.equal(expected)
+      done()
+    })
+    .catch(err => {
+      done(err)
     })
   })
 })
